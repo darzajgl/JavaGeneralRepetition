7c4a2363ec9a7db2f2170b581fd2d5bf445e85d5
Dariush <darzajgl@gmail.com>
2021-06-21 16:52:46 +0200

    Printer-encapsulation

--- src/Composition/MainComposition.java
+++ src/Composition/MainComposition.java
@@ -22,5 +22,20 @@ public class MainComposition {
         bedroom.makeBed();
         bedroom.getLamp().turnOn();
 
+//-------------------------------------------------
+        System.out.println("****************");
+        Printer printer = new Printer(50, true);
+        System.out.println(printer.addToner(50));
+        System.out.println("Initial page count = "+printer.getPagesPrinted());
+        int pagesPrinted = printer.printPages(5);
+        System.out.println("Pages printed was "+pagesPrinted + " new total print count for printer"+printer.getPagesPrinted());
+        pagesPrinted = printer.printPages(3);
+        System.out.println("Pages printed was "+pagesPrinted + " new total print count for printer"+printer.getPagesPrinted());
+
+
+
+
+
+
     }
 }
