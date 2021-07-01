6b1ea0c49db2033cd2e0b5da5fd41c07e5eae5c2
Dariush <darzajgl@gmail.com>
2021-07-01 15:51:14 +0200

    ReverseArray

--- src/Arrays/ArraysMain.java
+++ src/Arrays/ArraysMain.java
@@ -1,8 +1,15 @@
 package Arrays;
 
+import java.util.Arrays;
+
+import static Arrays.ReverseArray.reverse;
+
 public class ArraysMain {
     public static void main(String[] args) {
-
+        int[] newArray = new int[]{1, 2, 3, 4, 5};
+        System.out.println("The initial array is: " + Arrays.toString(newArray));
+        reverse(newArray);
+        System.out.println("The reversed array is: " + Arrays.toString(newArray));
     }
-
 }
+
---
+++ src/Arrays/ReverseArray.java
@@ -0,0 +1,23 @@
+package Arrays;
+/*
+Write a method called reverse() with an int array as a parameter.
+The method should not return any value. In other words, the method is allowed to modify the array parameter.
+To reverse the array, you have to swap the elements, so that the first element is swapped with the last element and so on.
+For example, if the array is [1, 2, 3, 4, 5], then the reversed array is [5, 4, 3, 2, 1].
+The method should first print out the newly passed in array as Array = [1, 2, 3, 4, 5]
+and then once it's been reversed, print it out as Reversed array = [5, 4, 3, 2, 1]
+
+*/
+
+public class ReverseArray {
+
+    public static void reverse(int[] array) {
+        int maxIndex = array.length - 1;
+        int halfLength = array.length / 2;
+        for (int i = 0; i < halfLength; i++) {
+            int temp = array[i];
+            array[i] = array[maxIndex - i];
+            array[maxIndex - i] = temp;
+        }
+    }
+}
